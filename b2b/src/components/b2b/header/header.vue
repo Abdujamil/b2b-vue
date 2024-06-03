5570eea004426e7abb0adeb835267e565b397149
Abdujamil <gafforovabdujamil@gmail.com>
2024-06-03 18:32:55 +0500

    update

--- b2b/src/components/b2b/header/header.vue
+++ b2b/src/components/b2b/header/header.vue
@@ -1,13 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+import Dropdown from 'primevue/dropdown';
+
 
 </script>
 
 <template>
-  <header class="header">
+  <header class="header" ref="header">
     <nav class="header__menu">
       <ul class="menu">
         <li class="menu__item">
           <a class="menu__link" href="#">B2B Рынок</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Биржа грузоперевозок</a>
         </li>
         <li class="menu__item">
           <a class="menu__link" href="#">Тендеры / Аукционы</a>
@@ -16,22 +22,28 @@
           <a class="menu__link" href="#">Соцсеть</a>
         </li>
         <li class="menu__item">
-          <a class="menu__link" href="#">Биржа фриланса</a>
+          <a class="menu__link" href="#">Фриланс</a>
         </li>
         <li class="menu__item">
-          <a class="menu__link" href="#">Помощь</a>
+          <a class="menu__link menu__link-help" href="#">Помощь</a>
         </li>
         <li class="menu__item">
-          <a class="menu__link" href="#">О нас</a>
+          <a class="menu__link" href="#">Тарифы</a>
         </li>
-        <li class="menu__item">
-          <a class="menu__link" href="#">Контакты</a>
+        <li class="menu__item menu__link-about" ref="aboutItem">
+          <a class="menu__link menu__link-help" href="#">О нас</a>
         </li>
+        <!-- <li class="dropdown">
+          <a href="#">Ещё</a>
+          <ul class="submenu">
+            <li><a href="#">Помощь</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">Контакты</a></li>
+          </ul>
+        </li> -->
       </ul>
       <a href="#" class="header__btn-join">Стать участником</a>
     </nav>
   </header>
-
 </template>
-
 <style scoped></style>
--- b2b/src/main.js
+++ b2b/src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
 
+import PrimeVue from 'primevue/config';
 import { createApp } from 'vue'
 import App from './App.vue'
 
