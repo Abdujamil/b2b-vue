bb1a2904b6fa195916c865b5e3f48bf04ef97595
Abdujamil <gafforovabdujamil@gmail.com>
2024-06-06 18:51:43 +0500

    upgrade

--- b2b/src/components/b2b/intro/intro-card/introCard.vue
+++ b2b/src/components/b2b/intro/intro-card/introCard.vue
@@ -1,6 +1,30 @@
 <script setup>
 import { defineProps } from 'vue';
 import arrhover from '@/assets/images/arrhover.svg';
+import axios from 'axios';
+
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: '/api/index.php?option=com_jshopping&controller=addon_api&section=product&task=list&args[limit]=20',
+  headers: { 
+    'Authorization': 'Bearer RmIF677BiWxLCUI2ImckQQ7RqUGrDjcA', 
+    'Cookie': '55e884495441419f79abfcee0eb88317=7eebfe30d9c231c6008e1ae54e071a5a'
+  }
+};
+
+axios.request(config)
+.then((response) => {
+//   const data = response.data;
+//   console.log(JSON.stringify(typeof(response.data)));
+//   console.log(JSON.stringify(response.data[result]));
+for (var res in response) {
+    console.log(JSON.stringify(response[res]['result']['parent_id']));
+}
+})
+.catch((error) => {
+    console.error('Error:', error.response ? error.response.data : error.message);
+});
 
 
 defineProps({
